ab2b922803c4cb16edd2c83eeb1f1767c710ba57
Anders Karlsen <karlsenanders@gmail.com>
2012-07-23 14:22:10 +0200

    Bomb? and neighbours

--- src/sweeper_game/core.clj
+++ src/sweeper_game/core.clj
@@ -5,7 +5,20 @@
   [& args]
   (println "Hello, World!"))
 
+(defn bomb? [pos board]
+  (let [y (first pos) x (second pos)]
+  (and (>= x 0) (>= y 0) (< y (count board)) (< x (count (board 0)))
+  (= :bomb ((board y) x))))
+  )
+
+(defn neighbours [pos]
+  (for [xd (range -1 2) yd (range -1 2)] [(+ yd (first pos)) (+ xd (second pos))])
+  )
+
 (defn open [pos board]
-  (if (= ((board (first pos)) (second pos)) :bomb) :bomb
-    1)
+  (if (bomb? pos board) :bomb
+    1
+;    (let [y (first pos) x (second pos)]
+;    (count (filter #(bomb? % board)))
+    )
   )
--- test/sweeper_game/core_test.clj
+++ test/sweeper_game/core_test.clj
@@ -2,6 +2,20 @@
   (:use clojure.test
         sweeper-game.core))
 
+(deftest test-find-bomb
+  (testing "That it indicates bombs on and off board"
+    (is (bomb? [1 1] [[0 0 0] [0 :bomb 0] [0 0 0]]) "A bomb is on the board")
+    (is (not (bomb?[1 0] [[0 0 0] [0 :bomb 0] [0 0 0]])) "No bomb there")
+    (is (not (bomb?[1 4] [[0 0 0] [0 :bomb 0] [0 0 0]])) "No bombs offboard")
+    )
+  )
+
+(deftest test-neighbours
+  (testing "Finding neighbours"
+    (is (= [[0 0] [1 0] [2 0] [0 1] [1 1] [2 1] [0 2] [1 2] [2 2]] (neighbours [1 1])))
+    )
+  )
+
 (deftest test-feedback
   (testing "That it gives the right feedback"
     (is (= :bomb (open [0 1] [[0 :bomb] [0 0]])) "Opening a bomb gives bomb")
