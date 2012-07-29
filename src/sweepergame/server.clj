edc7ed7d31b33f6a6de70b2561cecdcafa774bf2
Anders Karlsen <karlsenanders@hotmail.com>
2012-07-29 15:15:42 +0200

    Added display of board

--- src/sweepergame/server.clj
+++ src/sweepergame/server.clj
@@ -1,6 +1,7 @@
 (ns sweepergame.server
   (:use noir.core)
   (:use noir.request)
+  (:use sweepergame.core)
   (:require [noir.server :as server]))
 
 (defpage "/" []
@@ -8,6 +9,21 @@
 
 (defpage "/open" []
   (str ((ring-request) :params))
+  )
+
+(defn board-str [board]
+  (str "<html></body><table>"
+  (reduce str 
+    (map (fn [row] 
+      (str "<tr>" (reduce str 
+        (map #(str "<td>" (if (= :bomb %) "X" "0") "</td>") row)) 
+       "</tr>")
+   ) board)) 
+   "</table></body></html>")
+  )
+
+(defpage "/new" []
+  (board-str (random-board 8 8 10))
   )
 
 (defn -main [& m]
