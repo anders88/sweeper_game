6b83a1cfc09216f1b2801cb4afbcdb965a8bad18
Anders Karlsen <karlsenanders@gmail.com>
2012-10-15 21:06:46 +0200

    Displaying list using model

---
+++ resources/public/js/mainpage.js
@@ -0,0 +1,40 @@
+"use strict";
+
+$ (function() {
+
+    var ScoreRowModel = Backbone.Model.extend({});
+
+    var ScoreRowCollection = Backbone.Collection.extend({
+        model: ScoreRowModel
+    });
+
+    var ScoreRowView = Backbone.View.extend({
+        tagName: "li",
+
+        template: _.template($("#row-template").html()),
+
+        render: function() {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        }
+    });
+
+    var scores = new ScoreRowCollection;
+
+    scores.add([{point: "1"}, {point: "2"}]);
+
+    var ScoreTableView = Backbone.View.extend({
+        initialize: function() {
+            var scoreRow = $("#scoreRow");
+            scores.each(function(row) {
+                //scoreRow.append("<li>" + row.get("point") + "</li>");
+                var rowView = new ScoreRowView({model: row});
+                scoreRow.append(rowView.render().el);
+            });
+        }
+    });
+
+    var appView = new ScoreTableView({el: $("#scoreTable")});
+
+
+});
