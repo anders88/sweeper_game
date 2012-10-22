3a4d194bfc7779436ce3d38372a47a4b6e56cc85
Anders Karlsen <karlsenanders@gmail.com>
2012-10-22 20:03:02 +0200

    Fixed failing test

--- test/sweepergame/server_test.clj
+++ test/sweepergame/server_test.clj
@@ -2,6 +2,15 @@
   (:use clojure.test
         sweepergame.server))
 
+(defn reset-status [f]
+  (dosync (ref-set status {:numplayers 0 :players {}}))
+  (f)
+)
+
+(use-fixtures :each reset-status)
+
+
+
 (deftest test-read-coordinates
   (testing "That coordinate are read from the parameters"
     (is (= [1 2] (read-coordinates {:y "1" :x "2"})) "Reading coordinate")
@@ -29,9 +38,9 @@
 
 (deftest test-json-generation-of-score
   (testing "That that one score is correct"
-   (is (= "[{\"name\":\"Luke\",\"total\":300,\"finishedBoards\":2,\"maxOnBoard\":4},{\"name\":\"Darth\",\"total\":200,\"finishedBoards\":3,\"maxOnBoard\":5}]"
+   (is (= "[{\"name\":\"Luke\",\"total\":300,\"finishedBoards\":2,\"maxOnBoard\":4,\"bombed\":0},{\"name\":\"Darth\",\"total\":200,\"finishedBoards\":3,\"maxOnBoard\":5,\"bombed\":0}]"
           (gen-json-score
-                       [{:name "Luke" :points {:total 300 :finishedBoards 2 :maxOnBoard 4}}
-                        {:name "Darth" :points {:total 200 :finishedBoards 3 :maxOnBoard 5}}
+                       [{:name "Luke" :points {:total 300 :finishedBoards 2 :maxOnBoard 4 :bombed 0}}
+                        {:name "Darth" :points {:total 200 :finishedBoards 3 :maxOnBoard 5 :bombed 0}}
                         ])))
 ))
