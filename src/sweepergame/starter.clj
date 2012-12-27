982fcfc966e21b50a1b443430b918f3923cca1ce
Anders Karlsen <karlsenanders@gmail.com>
2012-12-27 21:49:37 +0100

    Bugfix. Parameters converted to int

--- src/sweepergame/starter.clj
+++ src/sweepergame/starter.clj
@@ -11,15 +11,19 @@
 (defn read-enviroment-variables [args]
   (if (and (second args) (.exists (new java.io.File (second args))))
     (apply hash-map (flatten (map keyval (clojure.string/split-lines (slurp (second args))))))
-    (let [res {:mode :dev :secured false :password-file "password.txt" :rows 16 :cols 30 :bombs 99 :hintsleep 400 :opensleep 150}]
+    (let [res {:mode :dev :secured false :password-file "password.txt" :rows "16" :cols "30" :bombs "99" :hintsleep "400" :opensleep "150"}]
     (println "Did not find setupfile. Using standard setup. Use 'lein run start <setupfile>' to supply a setupfile. Running with admin disabled - you must restart server manually")
     res)
 
   )
   )
 
+(defn conv-to-int [envmap paraname]
+	(assoc envmap paraname (Integer/parseInt (envmap paraname)))
+	)
+
 (defn read-env-add-defaults [args]
-	(assoc (read-enviroment-variables args) :allow-reopen false)
+	(assoc (-> (read-enviroment-variables args) (conv-to-int :rows) (conv-to-int :cols) (conv-to-int :bombs) (conv-to-int :hintsleep) (conv-to-int :opensleep)) :allow-reopen false)
 	)
 
 (defn update-password-file [enviroment-read]
